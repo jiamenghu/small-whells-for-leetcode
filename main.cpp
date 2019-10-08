152f6dcbac9c6ce1f6b5ba4442416b0e99d67a98
JMeng <48715711+jiamenghu@users.noreply.github.com>
2019-10-08 02:25:52 -0500

    主框架

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+
+// 粘贴leetcode Solution类
+
+
+//*****************//
+//粘贴打印函数
+
+
+//***************//
+//主函数
+int main()
+{
+   Solution test;
+
+   return 0;
+}
